f8906b1ac14c44bf9ce904a669a02152b3ef925d
Robert Debeljak <robertdebeljak.debeljak@gmail.com>
2021-04-19 17:29:09 -0700

    feat: implemented sorting

--- src/controllers/CodeController.ts
+++ src/controllers/CodeController.ts
@@ -1,20 +1,45 @@
-import { Request, Response, NextFunction } from 'express';
+import { NextFunction, Request, Response } from 'express';
+
 import NetworkOperatorData from '../models/NetworkOperatorData';
 import Country from '../models/Country';
+import { SORT_ORDER } from '../types';
 
 export async function getCodeList(req: Request, res: Response, next: NextFunction) {
   try {
-    const { limit: queryLimit, offset: queryOffset, search } = req.query;
+    const { limit: queryLimit, offset: queryOffset, search, sortBy = null, sortOrder = SORT_ORDER.DESC } = req.query;
 
     const limit: number = queryLimit ? parseInt(queryLimit as string, 10) : 50;
     const offset: number = queryOffset ? parseInt(queryOffset as string, 10) : 0;
     Country.find();
-    const query = NetworkOperatorData.find();
-
-    if (search) query.where('name', new RegExp(search as string, 'gi'));
-
+    const query = NetworkOperatorData.find().where('name', new RegExp(search as string, 'gi'));
     const totalCount = await NetworkOperatorData.count(query);
-    const data = await query.skip(offset * limit).limit(limit).populate('country').exec();
+    const data = await NetworkOperatorData.aggregate([
+      {
+        $match: {
+          name: new RegExp(search as string, 'gi')
+        }
+      },
+      {
+        $lookup: {
+          from: 'countries',
+          localField: 'country',
+          foreignField: '_id',
+          as: 'country',
+        },
+      },
+      {
+        $unwind: '$country',
+      },
+      {
+        $sort: { [sortBy as any]: sortOrder === SORT_ORDER.ASC ? 1 : (sortOrder === SORT_ORDER.DESC ? -1 : 0) }
+      },
+      {
+        $skip: offset * limit
+      },
+      {
+        $limit: limit,
+      },
+    ]);
 
     res.json({
       data,
